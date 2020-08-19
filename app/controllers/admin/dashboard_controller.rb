e711e0ec741da88d6d6079435b0652e9a24e63c4
NatalieSokolova <nataliesklv@gmail.com>
2020-08-19 22:47:31 +0000

    adds content product and category quantity to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_quantity = Product.count
+    @category_quantity = Category.count
   end
   
   http_basic_authenticate_with :name => ENV['username'], :password => ENV['password']
