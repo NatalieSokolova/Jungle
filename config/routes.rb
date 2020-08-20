5ceae76959e632d990420a649bc96cfe97c1f697
NatalieSokolova <nataliesklv@gmail.com>
2020-08-20 13:54:29 +0000

    creates view for signup

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     resources :categories, except: [:destroy, :edit, :update, :show]
   end
 
-  get 'about/' => 'about#show'
+  get '/about' => 'about#show'
 
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
