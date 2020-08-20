33ebb6db0d5bfea2bc328ea9a4d0638ebf1b3fb2
NatalieSokolova <nataliesklv@gmail.com>
2020-08-20 15:18:50 +0000

    adds login/logout btn to nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # methods to look up the user, if they're logged in, and save their user object to a variable
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  # allows us to use @current_user in our view files
   helper_method :current_user
 
+  # for sending user to the login page if they aren't logged in
   def authorize
     redirect_to '/login' unless current_user
   end
