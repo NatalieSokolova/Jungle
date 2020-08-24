30a354101a2b207679ad2c23d49db4c2fb44adfa
NatalieSokolova <nataliesklv@gmail.com>
2020-08-24 20:29:20 +0000

    adds test for validating Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,9 +3,34 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'should save successfully with all four fields set' do
-      @category = Category.new(name: 'Home Decor')
-      @product = Product.new(name: 'Candlestick', price: 39.99, quantity: 14, category: @category)
+      # .create Creates an object (or multiple objects) and saves it to the database, if validations pass. The resulting object is returned whether the object was saved successfully to the database or not, while .new only creates the local object but does not attempt to validate or save it to the DB.
+      @category = Category.create(name: 'Home Decor')
+      @product = Product.create(name: 'Candlestick', price: 39.99, quantity: 14, category: @category)
       expect(@product).to be_present
+    end
+
+    it 'shuold not save with name missing' do
+      @category = Category.create(name: 'Home Decor')
+      @product = Product.create(name: nil, price: 39.99, quantity: 14, category: @category)
+      expect(@product.errors.full_messages).to include "Name can't be blank"
+    end
+
+    it 'shuold not save with price missing' do
+      @category = Category.create(name: 'Home Decor')
+      @product = Product.create(name: 'Candlestick', price: nil, quantity: 14, category: @category)
+      expect(@product.errors.full_messages).to include "Price can't be blank"
+    end
+
+    it 'shuold not save with quantity missing' do
+      @category = Category.create(name: 'Home Decor')
+      @product = Product.create(name: 'Candlestick', price: 39.99, quantity: nil, category: @category)
+      expect(@product.errors.full_messages).to include "Quantity can't be blank"
+    end
+
+    it 'shuold not save with category missing' do
+      @category = Category.create(name: 'Home Decor')
+      @product = Product.create(name: 'Candlestick', price: 39.99, quantity: 14, category: nil)
+      expect(@product.errors.full_messages).to include "Category can't be blank"
     end
   end
 end
