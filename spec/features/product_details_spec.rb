550b0ce9477734e76e69f0692cba7f4628091bc3
NatalieSokolova <nataliesklv@gmail.com>
2020-08-25 20:07:24 +0000

    tests user ability to click on product details and view the product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product detail page by clicking on a product", type: :feature, js: true do
+  
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    # populates testing db with data
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "User clicks on product details" do
+    # ACT
+    visit root_path
+    page.first("article.product").hover.find("footer.actions").click_on("Details")
+
+    # DEBUG
+    save_screenshot('product_hover.png')
+
+    # VERIFY
+    expect(page).to have_css('article.product', count: 10)
+    expect(page).to have_text('64.99')
+    expect(page).to have_css 'section.products-show'
+    expect(page).to have_text('Quantity')
+  end
+
+  scenario "User sees product details" do
+    # ACT
+    visit "products/1"
+
+    # DEBUG
+    save_screenshot('product_details.png')
+
+    # VERIFY
+    expect(page).to have_css 'section.products-show'
+    expect(page).to have_text('Quantity')
+  end
+end
